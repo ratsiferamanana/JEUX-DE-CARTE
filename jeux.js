d010d5271db9d4f54b2d36c18b3bd9085a1dd103
unknown <hei.jimmy.5@gmail.com>
2024-01-19 17:42:31 +0300

    jeux.js

---
+++ jeux.js
@@ -0,0 +1,8 @@
+function multiply(nb1, nb2) {
+    return nb1 * nb2;
+  }
+  
+  // Appel de la fonction pour récupérer le résultat
+  let result = multiply(8, 4);
+  console.log("Le résultat de la multiplication est : "+ result); 
+  
